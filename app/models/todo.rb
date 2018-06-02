f68578341bc2306b27c328ea4348b1a2bece6759
コミットしました <yumayo14@gmail.com>
2018-06-02 17:22:09 +0900

    アソシエーションの設定

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,17 @@
 class Todo < ApplicationRecord
+  has_many :todo_from, class_name: "TodoRelation", foreign_key: "from_todo_id"
+  has_many :todo_to, class_name: "TodoRelation", foreign_key: "to_todo_id"
+  has_many :following, through: :todo_from, source: :to_todo
+  has_many :followed, through: :todo_to, source: :from_todo
+
+  accepts_nested_attributes_for :todo_from
+
+  def find_childrens
+    todo_childrens = []
+    self.following.ids.each do |matching_todo_id|
+      todo_childrens = Todo.where(id: matching_todo_id )
+    end
+    return todo_childrens
+  end
+
 end
