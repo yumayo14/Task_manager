ebe8ebe44535edec5ce88bddb5150389e2e09dab
コミットしました <yumayo14@gmail.com>
2018-06-04 11:55:05 +0900

    アソシエーション部分を分かりやすいように編集

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,8 +1,8 @@
 class Todo < ApplicationRecord
   has_many :todo_from, class_name: "TodoRelation", foreign_key: "from_todo_id"
   has_many :todo_to, class_name: "TodoRelation", foreign_key: "to_todo_id"
-  has_many :following, through: :todo_from, source: :to_todo
   has_many :followed, through: :todo_to, source: :from_todo
+  has_many :following, through: :todo_from, source: :to_todo
 
   accepts_nested_attributes_for :todo_to
 
