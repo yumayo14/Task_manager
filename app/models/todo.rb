6569b7c3185072c1b41e69024fc8f34fbdf119fb
コミットしました <yumayo14@gmail.com>
2018-06-07 15:47:58 +0900

    不要な記述の削除

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,8 +1,9 @@
 class Todo < ApplicationRecord
-  has_many :todo_from, class_name: "TodoRelation", foreign_key: "from_todo_id"
-  has_many :todo_to, class_name: "TodoRelation", foreign_key: "to_todo_id"
-  has_many :followed, through: :todo_to, source: :from_todo
+  has_many :todo_from, class_name: "TodoRelation", foreign_key: "from_todo_id", dependent: :destroy
   has_many :following, through: :todo_from, source: :to_todo
+
+  has_many :todo_to, class_name: "TodoRelation", foreign_key: "to_todo_id", dependent: :destroy
+  has_many :followed, through: :todo_to, source: :from_todo
 
   accepts_nested_attributes_for :todo_to
 
