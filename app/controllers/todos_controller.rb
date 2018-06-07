541fd9b364813ff4dada8edc6f1f8396dc8844f4
コミットしました <yumayo14@gmail.com>
2018-06-07 15:47:10 +0900

    Todosテーブルに[ title ]カラムを追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,8 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all.select{|todo| todo if todo.followed.blank? || todo.followed.ids == todo.id}
-      @todo = Todo.new
-      @todo.todo_to.build
+    @todo = Todo.new
+    @todo.todo_to.build
   end
 
   def new
@@ -16,9 +16,23 @@ class TodosController < ApplicationController
     end
   end
 
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    if @todo.todo_from.delete_all && @todo.delete || @todo.delete
+      redirect_to root_path
+    end
+  end
+
 private
   def todo_params
-    params.require(:todo).permit(:text, todo_to_attributes: [:id, :to_todo_id, :from_todo_id])
+    params.require(:todo).permit(:title, :text, todo_to_attributes: [:id, :to_todo_id, :from_todo_id])
   end
 
 end
