16258b3303dc7e472a859286c69d1ada9f641c39
コミットしました <yumayo14@gmail.com>
2018-06-09 14:26:10 +0900

    Todoモデルの編集

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,11 +1,5 @@
 class Todo < ApplicationRecord
-  has_many :todo_from, class_name: "TodoRelation", foreign_key: "from_todo_id", dependent: :destroy
-  has_many :following, through: :todo_from, source: :to_todo
-
-  has_many :todo_to, class_name: "TodoRelation", foreign_key: "to_todo_id", dependent: :destroy
-  has_many :followed, through: :todo_to, source: :from_todo
-
-  accepts_nested_attributes_for :todo_to
+  has_many :memos, dependent: :destroy
 
   validates :text, presence: true
 
